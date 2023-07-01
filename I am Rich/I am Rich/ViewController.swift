2a7e67347708d807dfeb70c69faf569dedaf9404
Vedant Arora <93697157+VedantArora18@users.noreply.github.com>
2023-07-01 09:23:13 +0530

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Vedant Arora on 12/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
